b6ef92de87634e04400a93c3a7325bf5c4642db4
huchangying <603021137@qq.com>
2023-10-09 14:20:20 +0800

    fix: add repository, fix download directory bug

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const path = require('path')
 
 const fetchTemplate = async (name) => {
   const templateSource = 'gitee.com:huchangkuang/react-template.git#master'
-  const tempPath = path.resolve(__dirname, `./${name}`)
+  const tempPath = path.resolve(process.cwd(), `./${name}`)
   if (fs.existsSync(tempPath)) {
-    fs.rmdirSync(tempPath)
+    await fs.rm(tempPath, { recursive: true })
   }
   fs.mkdir(tempPath, (err) => {
     if (err) {
-      console.log(error)
+      console.log(err)
       return;
     }
     const spinner = ora(`正在从远程仓库拉取远程模板...`).start()
-    download(templateSource, path.join(tempPath, name), { clone: true }, async error => {
+    download(templateSource, tempPath, { clone: true }, async error => {
       if (error) {
         console.log(error)
         spinner.color = 'red'
         spinner.fail('拉取远程模板仓库失败！')
-        fs.rmdirSync(tempPath)
+        await fs.rm(tempPath, { recursive: true })
         return;
       }
       spinner.color = 'green'
